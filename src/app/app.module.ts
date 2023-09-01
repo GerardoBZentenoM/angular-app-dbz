6a7c3db0f9440e767a1f96d9069378665f18a093
GerardoBZentenoM <brallanzenteno@gmail.com>
2023-09-01 13:44:52 -0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './dbz/pages/main-page.componen';
+
+@NgModule({
+  declarations: [AppComponent, MainPageComponent],
+  imports: [BrowserModule, CounterModule, HeroesModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
