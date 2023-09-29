5a70ecb1056f0f35d241cd0f264f22f79683ed86
GerardoBZentenoM <brallanzenteno@gmail.com>
2023-09-29 16:04:03 -0600

    avance en app

--- src/app/dbz/pages/main-page.componen.ts
+++ src/app/dbz/pages/main-page.componen.ts
@@ -9,11 +9,18 @@ export class MainPageComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
-
+  
   public characters: Character[] = [
     {
       name: 'Krilin',
       power: 500,
     },
+    {
+      name: "Goku",
+      power: 9500,
+    },
+    {name: "Vegeta",
+    power: 800,
+  }
   ];
 }
